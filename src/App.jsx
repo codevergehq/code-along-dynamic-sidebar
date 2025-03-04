10aa600b2588a74ee7c5118689ec8a3d4b3a041e
Alexander Schnebel <alexander.schnebel@codeverge.de>
2025-03-04 10:40:21 +0100

    final result

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,70 @@
-function App() {
+import { NavLink, Routes, Route } from "react-router";
+
+const Home = () => <h1>Home</h1>;
+const Profile = () => <h1>Profile</h1>;
+const Settings = () => <h1>Settings</h1>;
+
+const routes = [
+  {
+    path: "/",
+    main: () => <Home />,
+    sidebar: () => (
+      <p>
+        This is your home page. You'll see your feed which is made up of the
+        people you follow.
+      </p>
+    ),
+  },
+  {
+    path: "/profile",
+    main: () => <Profile />,
+    sidebar: () => (
+      <p>
+        This is your profile page. You'll be able to see all your profile
+        information as well as the people you follow.
+      </p>
+    ),
+  },
+  {
+    path: "/settings",
+    main: () => <Settings />,
+    sidebar: () => (
+      <p>
+        This is your settings page. You can change your name, image, and
+        anything else associated with your account.
+      </p>
+    ),
+  },
+];
+
+export default function App() {
   return (
     <div className="wrapper">
       <div className="sidebar">
-        <div className="nav">
-          
-        </div>
-      </div>
-    </div>
-  )
-}
+        <ul className="nav">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/profile">Profile</NavLink>
+          </li>
+          <li>
+            <NavLink to="/settings">Settings</NavLink>
+          </li>
+        </ul>
 
-export default App
+        <Routes>
+          {routes.map(({ path, sidebar }) => (
+            <Route key={path} path={path} element={sidebar()} />
+          ))}
+        </Routes>
+      </div>
+
+      <Routes>
+        {routes.map(({ path, main }) => (
+          <Route key={path} path={path} element={main()} />
+        ))}
+      </Routes>
+    </div>
+  );
+}
